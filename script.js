79fc4fbdbfdeb52c5dbb62d6f1d32a797827de61
MasterProtection <master.protection.nh@gmail.com>
2024-05-16 21:59:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.getElementById("upgradeBtn").addEventListener("click", function() {
-    // Placeholder function for upgrading Microsoft Defender
-    alert("Microsoft Defender has been upgraded successfully!");
-});
-
 document.getElementById("supportBtn").addEventListener("click", function() {
     // Redirect to the user-defined support link
     window.location.href = "YOUR_SUPPORT_LINK_HERE";
